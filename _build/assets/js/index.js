19223b07a67dca8fcb80478bdc0875883033a1e8
Nicolas "0swin" Roger <nicolas@oswin.io>
2019-06-09 00:03:11 +0200

    css and inline svg

--- _build/assets/js/index.js
+++ _build/assets/js/index.js
@@ -1,1 +1,20 @@
-"use strict";
+$('#collapseOne').on('shown.bs.collapse', function () {
+  $('html, body').animate({
+    scrollTop: $("#collapseOne").offset().top
+  }, 450);
+});
+$('#collapseTwo').on('shown.bs.collapse', function () {
+  $('html, body').animate({
+    scrollTop: $("#collapseTwo").offset().top
+  }, 450);
+});
+$('#collapseThree').on('shown.bs.collapse', function () {
+  $('html, body').animate({
+    scrollTop: $("#collapseThree").offset().top
+  }, 450);
+});
+$('#collapseFour').on('shown.bs.collapse', function () {
+  $('html, body').animate({
+    scrollTop: $("#collapseFour").offset().top
+  }, 450);
+});
