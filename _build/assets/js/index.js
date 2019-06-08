1f4309328194c8de9ef5be6cec7d3351dfdacb5a
Nicolas "0swin" Roger <nicolas@oswin.io>
2019-06-09 00:19:10 +0200

    Revert "Scroll fix JS"
    This reverts commit 2d0ad44d3883b529e0ef7df62c9ef1cea609ba05.

--- _build/assets/js/index.js
+++ _build/assets/js/index.js
@@ -1,20 +1,20 @@
 $('#collapseOne').on('shown.bs.collapse', function () {
   $('html, body').animate({
-    scrollTop: $("#headingOne").offset().top
+    scrollTop: $("#collapseOne").offset().top
   }, 450);
 });
 $('#collapseTwo').on('shown.bs.collapse', function () {
   $('html, body').animate({
-    scrollTop: $("#headingTwo").offset().top
+    scrollTop: $("#collapseTwo").offset().top
   }, 450);
 });
 $('#collapseThree').on('shown.bs.collapse', function () {
   $('html, body').animate({
-    scrollTop: $("#headingThree").offset().top
+    scrollTop: $("#collapseThree").offset().top
   }, 450);
 });
 $('#collapseFour').on('shown.bs.collapse', function () {
   $('html, body').animate({
-    scrollTop: $("#headingFour").offset().top
+    scrollTop: $("#collapseFour").offset().top
   }, 450);
 });
